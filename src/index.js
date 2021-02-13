630f86fb1420ff9dadbbb1c813763b82f2cc28af
fenixoleg7 <fenixoleg7@gmail.com>
2021-02-13 16:53:03 +0200

    feat: add solution for this task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
+  if(Array.isArray(array) && array.length ){
+    return Math.min.apply( Math, array );
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(Array.isArray(array) && array.length ){
+    return Math.max.apply( Math, array );
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(Array.isArray(array) && array.length ){
+    const sum = array.reduce((a, b) => a + b, 0);
+    return (sum / array.length) || 0;
+  }
   return 0;
 }
